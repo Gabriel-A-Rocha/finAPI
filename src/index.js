149cba039a48effcb2c0ebe4b3148677dde8e855
Gabriel Rocha <gabriel.altair.rocha@gmail.com>
2021-03-14 00:23:49 -0500

    feat: add account creation functionality

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
 const express = require("express");
+const { v4: uuid } = require("uuid");
 
 const app = express();
+
+app.use(express.json());
 
 const port = 3333;
 app.listen(port, () => console.log(`Server running at port ${port}`));
 
-app.get("/", (req, res) => {
-  return res.json({ msg: "Hi" });
+const customers = [];
+
+app.post("/account", (req, res) => {
+  const { cpf, name } = req.body;
+
+  if (customers.find((p) => p.cpf === cpf)) {
+    return res.json({ msg: "CPF already registered" });
+  }
+
+  const id = uuid();
+
+  const account = { id, name, cpf, statement: [] };
+
+  customers.push(account);
+
+  return res.status(201).json(account);
 });
